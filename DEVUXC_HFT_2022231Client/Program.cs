6f38ea213aa76e3d0917b9b6661cd1aafe340222
olipa <olipa@YOGA-260>
2022-10-30 17:06:53 +0100

    Add project files.

---
+++ DEVUXC_HFT_2022231Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DEVUXC_HFT_2022231Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
