c9b9cb65a32dcc99a010c0342ba8900e94c8f4e2
Olivér Farkas <olipadavan@gmail.com>
2022-10-31 11:57:12 +0100

    Projects created

---
+++ DEVUXC_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DEVUXC_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ DEVUXC_HFT_2022231.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DEVUXC_HFT_2022231.Test
+{
+    public class Class1
+    {
+    }
+}
