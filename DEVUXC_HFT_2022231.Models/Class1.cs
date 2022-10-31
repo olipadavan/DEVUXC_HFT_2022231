ec2e24c289783a3ef997560f23c807fbefab2993
Olivér Farkas <olipadavan@gmail.com>
2022-10-31 11:59:40 +0100

    Default classes deleted

--- DEVUXC_HFT_2022231.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DEVUXC_HFT_2022231.Models
-{
-    public class Class1
-    {
-    }
-}
--- DEVUXC_HFT_2022231.Repozitory/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DEVUXC_HFT_2022231.Repozitory
-{
-    public class Class1
-    {
-    }
-}
