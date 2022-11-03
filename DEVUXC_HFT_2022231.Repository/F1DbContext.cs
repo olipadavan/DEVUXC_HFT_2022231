0d722641f2ebc51ee3455b8b9e24f7998bce1b23
Olivér Farkas <olipadavan@gmail.com>
2022-11-03 11:56:24 +0100

    base Repository and DbContext created

---
+++ DEVUXC_HFT_2022231.Repository/F1DbContext.cs
@@ -0,0 +1,48 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Repository
+{
+    public class F1DbContext : DbContext
+    {
+        public virtual DbSet<Driver> Drivers { get; set; }
+        public virtual DbSet<Race> Races { get; set; }
+        public virtual DbSet<Season> Seasons { get; set; }
+        public F1DbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        public F1DbContext(DbContextOptions<F1DbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseInMemoryDatabase("carshopdb")
+                    .UseLazyLoadingProxies();
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Season>(entity =>
+            {
+                entity.HasMany(season => season.Races)
+                    .WithOne(races => races.Season)
+                    .HasForeignKey(race => race.SeasonId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            //Season thisyear = new Season() { Id = 2022 };
+        }
+    }
+}
---
+++ DEVUXC_HFT_2022231.Repository/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using DEVUXC_HFT_2022231.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Repository.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected F1DbContext ctx;
+
+        public Repository(F1DbContext ctx)
+        {
+            this.ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public virtual T Read(int id)
+        {
+            return ctx.Set<T>().FirstOrDefault(item => item.Id.Equals(id));
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public virtual void Update(T item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
+
