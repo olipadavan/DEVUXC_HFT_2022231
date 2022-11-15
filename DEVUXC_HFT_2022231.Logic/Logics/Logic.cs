8cd9201bb46b3fbe56f2119105975ddad0723c09
Oliver Farkas <olipadavan@gmail.com>
2022-11-15 15:12:30 +0100

    base logic created

---
+++ DEVUXC_HFT_2022231.Logic/Logics/Logic.cs
@@ -0,0 +1,13 @@
+﻿using DEVUXC_HFT_2022231.Logic.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Logic.Logics
+{
+    public class Logic
+    {
+    }
+}
