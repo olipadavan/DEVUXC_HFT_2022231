865824b3cc441992fd06498e056ab2b7247023ac
Oliver Farkas <olipadavan@gmail.com>
2022-11-15 15:12:11 +0100

    base logic created

---
+++ DEVUXC_HFT_2022231.Logic/Intefaces/ILogic.cs
@@ -0,0 +1,19 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Logic.Intefaces
+{
+    public interface ILogic<T>
+    {
+        void Create(T item);
+        void Delete(int item);
+        IEnumerable<T> ReadAll();
+        T Read(int id);
+        void Update(T item);
+    }
+}
