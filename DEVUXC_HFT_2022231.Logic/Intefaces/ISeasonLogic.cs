4cc704164ed2a5ae54a30b5e44f301bb40dea624
Oliver Farkas <olipadavan@gmail.com>
2022-11-16 21:34:28 +0100

    seasonlogic inteface created

---
+++ DEVUXC_HFT_2022231.Logic/Intefaces/ISeasonLogic.cs
@@ -0,0 +1,37 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Logic.Intefaces
+{
+    public class ISeasonLogic : ILogic<Season>
+    {
+        public void Create(Season item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Season Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Season> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Season item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
