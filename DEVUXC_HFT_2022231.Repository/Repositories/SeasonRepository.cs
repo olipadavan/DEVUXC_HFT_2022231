2a014d5f17735a9071fb6865b4abe58a76dcac1e
Oliver Farkas <olipadavan@gmail.com>
2022-11-23 18:12:14 +0100

    season repository implemented

---
+++ DEVUXC_HFT_2022231.Repository/Repositories/SeasonRepository.cs
@@ -0,0 +1,31 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DEVUXC_HFT_2022231.Repository.Interfaces;
+using System.Collections.Immutable;
+
+namespace DEVUXC_HFT_2022231.Repository.Repositories
+{
+    public class SeasonRepository : Repository<Season>, ISeasonRepository
+    {
+        public SeasonRepository(F1DbContext ctx) : base(ctx)
+        {
+        }
+
+        public IEnumerable<Point> GetPoint(int SeasonId, int DriverNumber)
+        {
+            var resoult = ctx.Seasons.Where(season => season.Id == SeasonId).Select(season => season.Standing.FirstOrDefault(d => d.DriverNumber == DriverNumber));
+            
+            return resoult;
+        }
+
+        public IEnumerable<Race> GetRaces(int SeasonId, int RaceId)
+        {
+            var resoult = ctx.Seasons.Where(s=> s.Id == SeasonId).Select(s => s.Races.Select(r => r.Id == RaceId));
+            return resoult;
+        }
+    }
+}
