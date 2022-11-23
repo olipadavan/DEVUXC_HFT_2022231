b4b30be894bcc102b6adfd8cc76eb1224873e6c1
Oliver Farkas <olipadavan@gmail.com>
2022-11-23 23:07:40 +0100

    Circuit repositiry update

--- DEVUXC_HFT_2022231.Repository/Interfaces/ICircuitRepository.cs
+++ DEVUXC_HFT_2022231.Repository/Interfaces/ICircuitRepository.cs
@@ -11,6 +11,11 @@ namespace DEVUXC_HFT_2022231.Repository.Interfaces
 {
     public interface ICircuitRepository : IRepository<Circuit>
     {
+        void Create(Race item);
+        void Delete(int item);
+        IEnumerable<Race> ReadAll();
+        Race Read(int id);
+        void Update(Race item);
         void trackChange(int CircuitId, double newLength);
         IQueryable<double> GetTrackLenght(int circuitId);
     }
