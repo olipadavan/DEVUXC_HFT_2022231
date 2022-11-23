b8ff6f68da4db89715dd8991d0d91f3953e23a81
Oliver Farkas <olipadavan@gmail.com>
2022-11-23 18:12:33 +0100

    Race Repository implemented

---
+++ DEVUXC_HFT_2022231.Repository/Repositories/RaceRepository.cs
@@ -0,0 +1,22 @@
+﻿using DEVUXC_HFT_2022231.Models;
+using DEVUXC_HFT_2022231.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231.Repository.Repositories
+{
+    internal class RaceRepository : Repository<Race>, IRaceRepository
+    {
+        public RaceRepository(F1DbContext ctx) : base(ctx)
+        {
+        }
+
+        public IQueryable<Driver> GetAllDrivers(int RaceId)
+        {
+            return ctx.Races.Where(r => r.Id == RaceId).Select(d => d.Drivers);
+        }
+    }
+}
