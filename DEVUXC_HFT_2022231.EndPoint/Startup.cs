c370e4498bb5e1db22aa0884de9f25ebd7197942
Oliver Farkas <olipadavan@gmail.com>
2022-11-24 15:56:42 +0100

    Endpoint Startup class implemented

--- DEVUXC_HFT_2022231.EndPoint/Startup.cs
+++ DEVUXC_HFT_2022231.EndPoint/Startup.cs
@@ -1,21 +1,50 @@
+using Castle.Core.Configuration;
+using DEVUXC_HFT_2022231.Logic.Intefaces;
+using DEVUXC_HFT_2022231.Logic.Logics;
+using DEVUXC_HFT_2022231.Repository;
+using DEVUXC_HFT_2022231.Repository.Interfaces;
+using DEVUXC_HFT_2022231.Repository.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace DEVUXC_HFT_2022231.EndPoint
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<F1DbContext>();
+            services.AddTransient<ISeasonRepository, SeasonRepository>();
+            services.AddTransient<IRaceRepository, RaceRepository>();
+            services.AddTransient<ICircuitRepository, CircuitRepository>();
+
+            services.AddTransient<ISeasonLogic, SeasonLogic>();
+            services.AddTransient<IRaceLogic, RaceLogic>();
+            services.AddTransient<ICircuitLogic, CircuitLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarShop.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +53,15 @@ namespace DEVUXC_HFT_2022231.EndPoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DEVUXC_HFT_2022231.EndPoint v1"));
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               endpoints.MapControllers();
             });
         }
     }
