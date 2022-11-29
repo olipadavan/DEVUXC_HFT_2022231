3b30035dedafffe0d2759c3948db192f0481c6fc
Oliver Farkas <olipadavan@gmail.com>
2022-11-29 13:22:03 +0100

    client class created

---
+++ DEVUXC_HFT_2022231Client/ClientProgram.cs
@@ -0,0 +1,18 @@
+﻿
+using DEVUXC_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVUXC_HFT_2022231Client
+{
+    public static class ClientProgram
+    {
+        public static void Main(string[] args)
+        {
+            F1DbContext dbctx = new F1DbContext();
+        }
+    }
+}
